1aa63fd73714c33b324c76fb1f6f2b61fbdd51f8
Omer Amsalem <88431895+Omer-Amsalem@users.noreply.github.com>
2025-03-19 12:18:51 +0200

    Business model

---
+++ server/models/businessModel.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const businessScheme = mongoose.Schema(
+    {
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    },
+    category:[String],
+
+    tokens:[
+        {
+            token:{
+                type: String,
+                required: true,
+            },
+            device_id:{
+                type: String,
+                required: true,
+            },
+            name:{
+                type: String,
+                required: true,
+            }
+        },
+    ],
+    rating:{
+        type: Number,
+        required: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    Image:{
+        type: String,
+        required: true,
+    },
+    currency:{
+        type: String,
+        required: true,
+    },
+}
+);
+mongoose.model("Business", businessScheme);
