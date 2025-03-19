2c97d9261ed58fead460f4002edf9829a8c7d204
Dekel Vaknin <dekelarlo@gmail.com>
2025-03-19 11:43:18 +0200

    item model

---
+++ server/models/itemModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const ItemScheme = mongoose.Schema(
+    {
+        business:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Business',
+            required: true,
+        },
+        description:{
+            type: String,
+            required: true,
+        },
+        amount:{
+            type: Number,
+            required: true,
+        },
+        price:{
+            type: Number,
+            required: true,
+        },
+        currency:{
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model('Item', ItemScheme);
