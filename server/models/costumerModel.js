f9e657f97c3c16cb93b39f86434676563a08ca24
Bar Larrea <barlarrea1@gmail.com>
2025-03-26 15:42:38 +0200

    Add verification fields to business and customer models

--- server/models/costumerModel.js
+++ server/models/costumerModel.js
@@ -1,41 +1,41 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const costumerScheme = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        phone: {
-            type: String,
-        },
-        address: {
-            type: String,
-        },
-        image: {
-            type: String,
-        },
-        creditCard: {
-            type: Object,
-        },
-        rating: {
-            type: Number,
-        },
-        role: {
-            type: String,
-            required: true,
-            default: 'Customer',
-        },
-        refreshTokens: [
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		phone: {
+			type: String,
+		},
+		address: {
+			type: String,
+		},
+		image: {
+			type: String,
+		},
+		creditCard: {
+			type: Object,
+		},
+		rating: {
+			type: Number,
+		},
+		role: {
+			type: String,
+			required: true,
+			default: 'Customer',
+		},
+		refreshTokens: [
 			{
 				token: {
 					type: String,
@@ -46,7 +46,22 @@ const costumerScheme = mongoose.Schema(
 				},
 			},
 		],
-    },
-    { timestamps: true }
+        verificationCode:{
+            type: String,
+        },
+		isValid: {
+			type: Boolean,
+			default: false,
+		},
+		isEmailValid: {
+			type: Boolean,
+			default: false,
+		},
+		isPaymentValid: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true }
 );
-export default mongoose.model("Costumer", costumerScheme);
+export default mongoose.model('Costumer', costumerScheme);
