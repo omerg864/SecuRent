5f2b64854509a59e0d91555a3907efdabac4ce73
96sahar <96saharyosef@gmail.com>
2025-04-08 11:49:03 +0300

    small fix

--- client/services/transactionService.ts
+++ client/services/transactionService.ts
@@ -21,4 +21,21 @@ const chargeDeposit = async (
   }
 };
 
-export { chargeDeposit };
+const createTransactionFromItem = async (itemId: string) => {
+  try {
+    const accessToken = await checkToken();
+    console.log("Access Token:", accessToken);
+    const response = await client.post<{ success: boolean }>(
+      `transaction/${itemId}`,
+      {},
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    return response.data.success;
+  } catch (error) {
+    throw error || "Transaction creation failed.";
+  }
+};
+
+export { chargeDeposit, createTransactionFromItem };
