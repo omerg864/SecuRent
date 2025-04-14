3525a9155a5de38d6aa0c76353a1f074d769c705
Bar Larrea <barlarrea1@gmail.com>
2025-04-14 23:43:33 +0300

    Refactor transactionModel.js schema definition

--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -1,60 +1,57 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const transactionScheme = mongoose.Schema(
-	{
-		transaction_id: {
-			type: String,
-		},
-		stripe_payment_id: {
-			type: String,
-			required: true,
-		  },		  
-		amount: {
-			type: Number,
-			required: true,
-		},
-		description: {
-			type: String,
-			required: true,
-		},
-		currency: {
-			type: String,
-			required: true,
-		},
-		status: {
-			type: String,
-			required: true,
-		},
-		business: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Business',
-			required: true,
-		},
-		customer: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Customer',
-		},
-		opened_at: {
-			type: Date,
-			default: Date.now,
-		},
-		closed_at: {
-			type: Date,
-		},
-		return_date: {
-			type: Date,
-		},
-		charged: {
-			type: Number,
-		},
-		charged_description: {
-			type: String,
-		},
-		client_secret: {
-			type: String
-		  }
-	},
-	{ timestamps: true }
+  {
+    stripe_payment_id: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    currency: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    business: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Business",
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+    opened_at: {
+      type: Date,
+      default: Date.now,
+    },
+    closed_at: {
+      type: Date,
+    },
+    return_date: {
+      type: Date,
+    },
+    charged: {
+      type: Number,
+    },
+    charged_description: {
+      type: String,
+    },
+    client_secret: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model('Transaction', transactionScheme);
+export default mongoose.model("Transaction", transactionScheme);
