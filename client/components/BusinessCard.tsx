2e50fd83452c9f2fc37747a008be36e3817fafcc
Omer Gaiznger <Omerg863@gmail.com>
2025-05-01 20:26:13 +0300

    distance sort and business image and categories fix

--- client/components/BusinessCard.tsx
+++ client/components/BusinessCard.tsx
@@ -2,30 +2,33 @@ import { Image, Text, View } from 'react-native';
 import StarRating from './StarRating';
 import HapticButton from '@/components/ui/HapticButton';
 import { Business } from '@/services/interfaceService';
-import * as Haptics from "expo-haptics";
+import * as Haptics from 'expo-haptics';
 
 interface BusinessCardProps {
 	business: Business;
 	onPress: () => void;
 }
 
-const BusinessCard: React.FC<BusinessCardProps> = ({   onPress, business }) => {
-	  const handlePress = () => {
+const BusinessCard: React.FC<BusinessCardProps> = ({ onPress, business }) => {
+	const handlePress = () => {
 		Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
 		onPress();
-	  };
+	};
 	return (
 		<HapticButton
 			className="bg-indigo-700 rounded-lg p-4 mb-4"
 			onPress={handlePress}
 		>
 			<View className="flex-row items-center">
-				<View className="w-12 h-12 bg-white rounded-full items-center justify-center mr-3">
-					<Image
-						source={require('../assets/images/location-icon.png')}
-						className="w-8 h-8"
-					/>
-				</View>
+				<Image
+					src={business.image ? business.image : undefined}
+					source={
+						business.image
+							? undefined
+							: require('../assets/images/location-icon.png')
+					}
+					className="w-12 h-12 rounded-full mr-3 bg-white"
+				/>
 				<View className="flex-1">
 					<View className="flex-row justify-between items-center">
 						<Text className="text-white font-bold text-lg">
@@ -41,7 +44,11 @@ const BusinessCard: React.FC<BusinessCardProps> = ({   onPress, business }) => {
 				</View>
 			</View>
 			<View className="flex-row justify-between mt-2">
-				<Text className="text-white">{business.category}</Text>
+				<Text className="text-white">
+					{business.category && business.category.length > 0
+						? business.category[0]
+						: ''}
+				</Text>
 				<StarRating rating={business.rating.overall} />
 			</View>
 		</HapticButton>
