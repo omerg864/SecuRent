25d6ec7f7296e00bcadd0105c827853be6dcf6cc
96sahar <96saharyosef@gmail.com>
2025-05-15 22:18:12 +0300

    report-button-display-change

--- client/app/sharedLayouts/BusinessProfileScreen.tsx
+++ client/app/sharedLayouts/BusinessProfileScreen.tsx
@@ -23,7 +23,6 @@ import UserImage from '@/components/UserImage';
 import { BusinessDetails } from '@/services/interfaceService';
 import { currencies } from '@/utils/constants';
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
-import { BaseModal } from '@/components/BaseModal';
 import WarningPopupModal from '@/components/ui/WarningPopUpModal';
 
 const BusinessProfileScreen = () => {
@@ -187,328 +186,349 @@ const BusinessProfileScreen = () => {
 	const { business, items = [], reviews = [] } = businessData;
 
 	return (
-		<ParallaxScrollView
-			headerImage={
-				business.image ? (
-					<Image
-						source={{ uri: business.image }}
-						className="w-full h-full"
-						resizeMode="cover"
-					/>
-				) : (
-					<View className="w-full h-full justify-center items-center bg-gray-400">
-						<Ionicons
-							name="business-outline"
-							size={108}
-							color="white"
+		<>
+			<View className="absolute top-6 right-5 z-10">
+				{accountType === 'personal' && (
+					<HapticButton
+						onPress={handleOpenReportModal}
+						className="bg-gray-100 px-4 py-2 rounded-full"
+					>
+						<ThemedText
+							style={{ color: '#dc2626', fontWeight: '600' }}
+						>
+							Report
+						</ThemedText>
+					</HapticButton>
+				)}
+			</View>
+			<ParallaxScrollView
+				headerImage={
+					business.image ? (
+						<Image
+							source={{ uri: business.image }}
+							className="w-full h-full"
+							resizeMode="cover"
 						/>
-					</View>
-				)
-			}
-			headerBackgroundColor={{ light: '#ffffff', dark: '#ffffff' }}
-			onBack={() => {
-				if (typeof params.from === 'string') {
-					router.replace({
-						pathname: params.from as RelativePathString,
-					});
-				} else {
-					router.back();
+					) : (
+						<View className="w-full h-full justify-center items-center bg-gray-400">
+							<Ionicons
+								name="business-outline"
+								size={108}
+								color="white"
+							/>
+						</View>
+					)
 				}
-			}}
-		>
-			<ScrollView className="bg-white">
-				<ThemedView className="px-4 py-6" darkColor="white">
-					<View className="flex-row justify-between w-full">
-						<ThemedText
-							type="title"
-							className="text-2xl font-bold text-black"
-							darkColor="black"
-						>
-							{business.name}
-						</ThemedText>
-						{accountType === 'personal' && (
-							<HapticButton onPress={handleOpenReportModal}>
-								<Ionicons
-									name="warning-outline"
-									color="red"
-									size={32}
-								/>
-							</HapticButton>
-						)}
-					</View>
-					<View className="flex-row items-center my-3">
-						<StarRating
-							rating={business.rating.overall}
-							size={20}
-						/>
-						<ThemedText
-							className="ml-2 text-black"
-							darkColor="black"
-						>
-							{business.rating.overall.toFixed(1)}
-						</ThemedText>
-					</View>
-
-					{business.category?.length > 0 && (
-						<View className="flex-row items-center mb-3">
-							<MaterialIcons
-								name="category"
-								size={18}
-								color="#666"
+				headerBackgroundColor={{ light: '#ffffff', dark: '#ffffff' }}
+				onBack={() => {
+					if (typeof params.from === 'string') {
+						router.replace({
+							pathname: params.from as RelativePathString,
+						});
+					} else {
+						router.back();
+					}
+				}}
+			>
+				<ScrollView className="bg-white">
+					<ThemedView className="px-4 py-6" darkColor="white">
+						<View className="flex-row justify-between w-full">
+							<ThemedText
+								type="title"
+								className="text-2xl font-bold text-black"
+								darkColor="black"
+							>
+								{business.name}
+							</ThemedText>
+						</View>
+						<View className="flex-row items-center my-3">
+							<StarRating
+								rating={business.rating.overall}
+								size={20}
 							/>
 							<ThemedText
 								className="ml-2 text-black"
 								darkColor="black"
 							>
-								{business.category[0]}
+								{business.rating.overall.toFixed(1)}
 							</ThemedText>
 						</View>
-					)}
 
-					<View className="flex-row items-start justify-between mb-4">
-						<Ionicons
-							name="location-outline"
-							size={18}
-							color="#666"
-						/>
-						<ThemedText
-							className="ml-2 flex-1 text-black"
-							darkColor="black"
-						>
-							{business.address}
-						</ThemedText>
-						<HapticButton
-							onPress={handleNavigate}
-							className="bg-blue-500 rounded-full p-2 ml-2"
-						>
-							<Ionicons name="navigate" size={20} color="white" />
-						</HapticButton>
-					</View>
-
-					<View className="flex-row items-center justify-between mb-4">
-						<Ionicons name="call-outline" size={18} color="#666" />
-						<ThemedText
-							className="ml-2 flex-1 text-black"
-							darkColor="black"
-						>
-							{business.phone}
-						</ThemedText>
-						<HapticButton
-							onPress={handleCall}
-							className="bg-green-500 rounded-full p-2 ml-2"
-						>
-							<Ionicons name="call" size={20} color="white" />
-						</HapticButton>
-					</View>
-				</ThemedView>
-
-				{/* Tabs */}
-				<View className="flex-row border-b border-gray-300">
-					{['items', 'reviews'].map((tab) => (
-						<HapticButton
-							key={tab}
-							onPress={() => setActiveTab(tab)}
-							className={`flex-1 py-3 ${
-								activeTab === tab
-									? 'border-b-2 border-[#4338CA]'
-									: ''
-							}`}
-						>
-							<ThemedText
-								className={`text-center ${
-									activeTab === tab
-										? 'font-bold text-blue-500'
-										: 'text-black'
-								}`}
-								darkColor="black"
-							>
-								{tab.charAt(0).toUpperCase() + tab.slice(1)}
-							</ThemedText>
-						</HapticButton>
-					))}
-				</View>
-
-				{activeTab === 'items' ? (
-					<View className="p-4 bg-white">
-						{items.length === 0 ? (
-							<View className="items-center justify-center py-10">
+						{business.category?.length > 0 && (
+							<View className="flex-row items-center mb-3">
+								<MaterialIcons
+									name="category"
+									size={18}
+									color="#666"
+								/>
 								<ThemedText
-									type="defaultSemiBold"
-									className="text-gray-400 text-lg"
-									darkColor="gray"
+									className="ml-2 text-black"
+									darkColor="black"
 								>
-									No items yet...
+									{business.category[0]}
 								</ThemedText>
 							</View>
-						) : (
-							items.map((item: any) => (
-								<HapticButton
-									key={item._id}
-									onPress={() => handleItemPress(item)}
-									className="flex-row mb-6 bg-white rounded-lg p-3 shadow items-center"
-								>
-									<View className="flex-row items-center flex-1">
-										<Image
-											source={{ uri: item.image }}
-											className="w-20 h-20 rounded-lg"
-											resizeMode="cover"
-										/>
-										<View className="ml-3 flex-1">
-											<ThemedText
-												type="defaultSemiBold"
-												className="text-black mb-1"
-												darkColor="black"
-											>
-												{item.description}
-											</ThemedText>
+						)}
 
-											<View>
+						<View className="flex-row items-start justify-between mb-4">
+							<Ionicons
+								name="location-outline"
+								size={18}
+								color="#666"
+							/>
+							<ThemedText
+								className="ml-2 flex-1 text-black"
+								darkColor="black"
+							>
+								{business.address}
+							</ThemedText>
+							<HapticButton
+								onPress={handleNavigate}
+								className="bg-blue-500 rounded-full p-2 ml-2"
+							>
+								<Ionicons
+									name="navigate"
+									size={20}
+									color="white"
+								/>
+							</HapticButton>
+						</View>
+
+						<View className="flex-row items-center justify-between mb-4">
+							<Ionicons
+								name="call-outline"
+								size={18}
+								color="#666"
+							/>
+							<ThemedText
+								className="ml-2 flex-1 text-black"
+								darkColor="black"
+							>
+								{business.phone}
+							</ThemedText>
+							<HapticButton
+								onPress={handleCall}
+								className="bg-green-500 rounded-full p-2 ml-2"
+							>
+								<Ionicons name="call" size={20} color="white" />
+							</HapticButton>
+						</View>
+					</ThemedView>
+
+					{/* Tabs */}
+					<View className="flex-row border-b border-gray-300">
+						{['items', 'reviews'].map((tab) => (
+							<HapticButton
+								key={tab}
+								onPress={() => setActiveTab(tab)}
+								className={`flex-1 py-3 ${
+									activeTab === tab
+										? 'border-b-2 border-[#4338CA]'
+										: ''
+								}`}
+							>
+								<ThemedText
+									className={`text-center ${
+										activeTab === tab
+											? 'font-bold text-blue-500'
+											: 'text-black'
+									}`}
+									darkColor="black"
+								>
+									{tab.charAt(0).toUpperCase() + tab.slice(1)}
+								</ThemedText>
+							</HapticButton>
+						))}
+					</View>
+
+					{activeTab === 'items' ? (
+						<View className="p-4 bg-white">
+							{items.length === 0 ? (
+								<View className="items-center justify-center py-10">
+									<ThemedText
+										type="defaultSemiBold"
+										className="text-gray-400 text-lg"
+										darkColor="gray"
+									>
+										No items yet...
+									</ThemedText>
+								</View>
+							) : (
+								items.map((item: any) => (
+									<HapticButton
+										key={item._id}
+										onPress={() => handleItemPress(item)}
+										className="flex-row mb-6 bg-white rounded-lg p-3 shadow items-center"
+									>
+										<View className="flex-row items-center flex-1">
+											<Image
+												source={{ uri: item.image }}
+												className="w-20 h-20 rounded-lg"
+												resizeMode="cover"
+											/>
+											<View className="ml-3 flex-1">
 												<ThemedText
 													type="defaultSemiBold"
-													className="text-green-600"
+													className="text-black mb-1"
 													darkColor="black"
 												>
-													{item.price}{' '}
-													{currencies.find(
-														(currency) =>
-															currency.code ===
-															item.currency
-													)?.symbol || '₪'}
+													{item.description}
 												</ThemedText>
-												<View className="flex-row items-center mt-1">
-													<Ionicons
-														name="time-outline"
-														size={14}
-														color="#666"
-													/>
+
+												<View>
 													<ThemedText
-														className="text-xs ml-1 text-gray-500"
+														type="defaultSemiBold"
+														className="text-green-600"
 														darkColor="black"
 													>
-														{item.duration}{' '}
-														{item.timeUnit}
+														{item.price}{' '}
+														{currencies.find(
+															(currency) =>
+																currency.code ===
+																item.currency
+														)?.symbol || '₪'}
 													</ThemedText>
+													<View className="flex-row items-center mt-1">
+														<Ionicons
+															name="time-outline"
+															size={14}
+															color="#666"
+														/>
+														<ThemedText
+															className="text-xs ml-1 text-gray-500"
+															darkColor="black"
+														>
+															{item.duration}{' '}
+															{item.timeUnit}
+														</ThemedText>
+													</View>
 												</View>
 											</View>
 										</View>
-									</View>
 
-									{isBusiness && (
-										<View className="flex-row ml-4">
-											<HapticButton
-												onPress={() => handleEdit(item)}
-												className="bg-[#4338CA] rounded-full p-2.5"
-											>
-												<Ionicons
-													name="create-outline"
-													size={20}
-													color="white"
-												/>
-											</HapticButton>
-											<HapticButton
-												onPress={() =>
-													handleDelete(item)
-												}
-												className="bg-[#4338CA] rounded-full p-2.5 ml-2"
-											>
-												<Ionicons
-													name="trash-outline"
-													size={20}
-													color="white"
-												/>
-											</HapticButton>
-											<HapticButton
-												onPress={() =>
-													handleQRCode(item)
-												}
-												className="bg-[#4338CA] rounded-full p-2.5 ml-2"
-											>
-												<Ionicons
-													name="qr-code-outline"
-													size={20}
-													color="white"
-												/>
-											</HapticButton>
-										</View>
-									)}
-								</HapticButton>
-							))
-						)}
-					</View>
-				) : (
-					<View className="p-4">
-						<ThemedText
-							className="font-semibold mb-2 text-black"
-							darkColor="black"
-						>
-							Review Summary
-						</ThemedText>
-						<ThemedText
-							className="text-gray-500 mb-4"
-							darkColor="black"
-						>
-							{business.reviewSummary || 'No reviews yet...'}
-						</ThemedText>
-						{reviews.map((review: any) => {
-							const createdAt = new Date(
-								review.createdAt
-							).toLocaleDateString('en-GB', {
-								day: '2-digit',
-								month: '2-digit',
-								year: 'numeric',
-							});
+										{isBusiness && (
+											<View className="flex-row ml-4">
+												<HapticButton
+													onPress={() =>
+														handleEdit(item)
+													}
+													className="bg-[#4338CA] rounded-full p-2.5"
+												>
+													<Ionicons
+														name="create-outline"
+														size={20}
+														color="white"
+													/>
+												</HapticButton>
+												<HapticButton
+													onPress={() =>
+														handleDelete(item)
+													}
+													className="bg-[#4338CA] rounded-full p-2.5 ml-2"
+												>
+													<Ionicons
+														name="trash-outline"
+														size={20}
+														color="white"
+													/>
+												</HapticButton>
+												<HapticButton
+													onPress={() =>
+														handleQRCode(item)
+													}
+													className="bg-[#4338CA] rounded-full p-2.5 ml-2"
+												>
+													<Ionicons
+														name="qr-code-outline"
+														size={20}
+														color="white"
+													/>
+												</HapticButton>
+											</View>
+										)}
+									</HapticButton>
+								))
+							)}
+						</View>
+					) : (
+						<View className="p-4">
+							<ThemedText
+								className="font-semibold mb-2 text-black"
+								darkColor="black"
+							>
+								Review Summary
+							</ThemedText>
+							<ThemedText
+								className="text-gray-500 mb-4"
+								darkColor="black"
+							>
+								{business.reviewSummary || 'No reviews yet...'}
+							</ThemedText>
+							{reviews.map((review: any) => {
+								const createdAt = new Date(
+									review.createdAt
+								).toLocaleDateString('en-GB', {
+									day: '2-digit',
+									month: '2-digit',
+									year: 'numeric',
+								});
 
-							return (
-								<HapticButton
-									key={review._id}
-									onPress={() => handleReviewPress(review)}
-									className="bg-white rounded-lg p-4 mb-4 shadow"
-								>
-									<View className="flex-row justify-between items-center mb-2">
-										<View className="flex-row items-center">
-											<UserImage
-												image={review.customer.image}
-												name={review.customer.name}
-												size={12}
-											/>
+								return (
+									<HapticButton
+										key={review._id}
+										onPress={() =>
+											handleReviewPress(review)
+										}
+										className="bg-white rounded-lg p-4 mb-4 shadow"
+									>
+										<View className="flex-row justify-between items-center mb-2">
+											<View className="flex-row items-center">
+												<UserImage
+													image={
+														review.customer.image
+													}
+													name={review.customer.name}
+													size={12}
+												/>
+												<ThemedText
+													className="ml-2 font-semibold text-black"
+													darkColor="black"
+												>
+													{review.customer.name}
+												</ThemedText>
+											</View>
 											<ThemedText
-												className="ml-2 font-semibold text-black"
-												darkColor="black"
+												className="text-xs text-gray-400"
+												darkColor="gray"
 											>
-												{review.customer.name}
+												{createdAt}
 											</ThemedText>
 										</View>
 										<ThemedText
-											className="text-xs text-gray-400"
-											darkColor="gray"
+											className="text-gray-700"
+											darkColor="black"
 										>
-											{createdAt}
+											{review.content.trim()}
 										</ThemedText>
-									</View>
-									<ThemedText
-										className="text-gray-700"
-										darkColor="black"
-									>
-										{review.content.trim()}
-									</ThemedText>
-								</HapticButton>
-							);
-						})}
-					</View>
-				)}
-			</ScrollView>
-			<WarningPopupModal
-				visible={reportModalVisible}
-				onClose={handleCloseReportModal}
-				onConfirm={handleAcceptReporting}
-				title="Report Business"
-				message={`Do you want to file a report for ${business.name}`}
-				confirmText="Yes"
-				cancelText="No"
-				iconName="warning"
-				iconColor="red"
-			/>
-		</ParallaxScrollView>
+									</HapticButton>
+								);
+							})}
+						</View>
+					)}
+				</ScrollView>
+				<WarningPopupModal
+					visible={reportModalVisible}
+					onClose={handleCloseReportModal}
+					onConfirm={handleAcceptReporting}
+					title="Report Business"
+					message={`Do you want to file a report for ${business.name}`}
+					confirmText="Yes"
+					cancelText="No"
+					iconName="warning"
+					iconColor="red"
+				/>
+			</ParallaxScrollView>
+		</>
 	);
 };
 
