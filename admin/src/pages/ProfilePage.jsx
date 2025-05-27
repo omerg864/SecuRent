6a52e17072e32bc9d20fc1691148866cd855fc0c
96sahar <96saharyosef@gmail.com>
2025-05-27 11:33:51 +0300

    fix for route import

---
+++ admin/src/pages/ProfilePage.jsx
@@ -0,0 +1,8 @@
+const ProfilePage = () => {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<p className="text-red-500 text-4xl">Hello Profile</p>
+		</div>
+	);
+};
+export default ProfilePage;
